16a0b35e45f561846e27a98eac0d5873c52bae98
Peter Ton <mail@peterton.nl>
2018-11-07 22:50:42 +0100

    Play around with some icons

--- src/Infrastructure/Controller/HomeController.php
+++ src/Infrastructure/Controller/HomeController.php
@@ -5,10 +5,11 @@ namespace App\Infrastructure\Controller;
 use App\Application\Query\WeatherDataQuery;
 use App\Application\QueryHandler\WeatherQueryHandler;
 use App\Infrastructure\Locator\IpLocator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController
+class HomeController extends Abstractcontroller
 {
     /**
      * @var IpLocator
@@ -30,12 +31,12 @@ class HomeController
 
     public function index(Request $request)
     {
+        return $this->render('test.html.twig');
         $ip = $request->getClientIp();
         $location = $this->ipLocator->getLocationForIp($ip);
         $data = $this->queryHandler->getWeatherDataByQuery(
             new WeatherDataQuery($location->lat, $location->lon)
         );
         var_dump($data);
-        return new Response('hoi');
     }
 }
