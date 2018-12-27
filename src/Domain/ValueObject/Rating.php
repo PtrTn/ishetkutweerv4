e18ad4b9881af32a28b858ce0ce2f3ff1da7a780
Peter Ton <mail@peterton.nl>
2018-12-27 21:42:21 +0100

    Refactor rating to use constants instead of hardcoded ints

--- src/Domain/ValueObject/Rating.php
+++ src/Domain/ValueObject/Rating.php
@@ -4,34 +4,42 @@ namespace App\Domain\ValueObject;
 
 class Rating
 {
+    public const MEGA_KUT = 4;
+
+    public const KUT = 3;
+
+    public const BEETJE_KUT = 2;
+
+    public const NIET_KUT = 1;
+
     /**
      * @var int
      */
     private $rating;
 
-    private function __construct(int $rating)
+    public function __construct(int $rating)
     {
         $this->rating = $rating;
     }
 
     public static function megaKut(): self
     {
-        return new self(4);
+        return new self(self::MEGA_KUT);
     }
 
     public static function kut(): self
     {
-        return new self(3);
+        return new self(self::KUT);
     }
 
     public static function beetjeKut(): self
     {
-        return new self(2);
+        return new self(self::BEETJE_KUT);
     }
 
     public static function nietKut(): self
     {
-        return new self(1);
+        return new self(self::NIET_KUT);
     }
 
     public function getRating(): int {
