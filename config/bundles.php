e42a56fbb9a0435852435847ec59fcb6ad22f261
Peter Ton <peterton91@gmail.com>
2020-09-26 01:41:54 +0200

    Update to symfony 5

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Eko\FeedBundle\EkoFeedBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
